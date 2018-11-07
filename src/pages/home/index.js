34dafc0d3c9a1aa789ee9dfdc93001c6dbaf3185
zhouhuafei <1123486116@qq.com>
2018-11-07 16:18:38 +0800

    四个页面

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -52,22 +52,22 @@ export default class Index extends Component {
         }
         <View className="g-footer-nav-wrap">
           <View className="g-footer-nav">
-            <View className="g-footer-nav-item">
+            <Navigator url="/pages/home/index" className="g-footer-nav-item">
               <View className="g-footer-nav-item-text">官方首页</View>
               <View className="g-footer-nav-item-desc">Home page</View>
-            </View>
-            <View className="g-footer-nav-item">
+            </Navigator>
+            <Navigator url="/pages/service-content/index" className="g-footer-nav-item">
               <View className="g-footer-nav-item-text">服务内容</View>{/*富文本*/}
               <View className="g-footer-nav-item-desc">Service content</View>
-            </View>
-            <View className="g-footer-nav-item">
+            </Navigator>
+            <Navigator url="/pages/about-us/index" className="g-footer-nav-item">
               <View className="g-footer-nav-item-text">关于我们</View>{/*富文本*/}
               <View className="g-footer-nav-item-desc">About us</View>
-            </View>
-            <View className="g-footer-nav-item">
+            </Navigator>
+            <Navigator url="/pages/contact-us/index" className="g-footer-nav-item">
               <View className="g-footer-nav-item-text">联系我们</View>
               <View className="g-footer-nav-item-desc">Contact us</View>
-            </View>
+            </Navigator>
           </View>
         </View>
       </View>
