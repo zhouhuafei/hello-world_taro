61e867c016ae717a3e0e70243c1a6f861df5ca53
zhouhuafei <1123486116@qq.com>
2025-08-07 22:19:09 +0800

    feat: 🎸 useEffect

--- src/components/ScratchCard/index.tsx
+++ src/components/ScratchCard/index.tsx
@@ -1,7 +1,17 @@
 import css from './index.module.scss'
 import { View } from '@tarojs/components'
+import { useEffect, useState } from 'react'
 
-function Index () {
+function Index (props) {
+  console.log('======props', props)
+  const [resData, setResData] = useState({})
+  console.log('======resData', resData)
+
+  useEffect(() => {
+    console.log('======useEffect')
+    setResData((prev) => ({ ...prev, a: 1, b: 2 }))
+  }, [])
+
   return (
     <View className={css.container}>
       View
